ff59f1a44e30af0fb666d675815823017505365f
Isaacvera394 <167934670+Isaacvera394@users.noreply.github.com>
2024-04-23 19:50:03 -0400

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,83 @@
+function ocultar() {
+	document.getElementById('oculto').style.display = 'none';
+	document.getElementById('despliegue').style.display = 'block';
+	document.getElementById('detalle').style.display = 'none';
+	}
+
+function Mostrar() {
+	document.getElementById('oculto').style.display = 'block';
+	document.getElementById('despliegue').style.display = 'none';
+	document.getElementById('detalle').style.display = 'block';
+}
+
+function ocultari() {
+	document.getElementById('ocultoi').style.display = 'none';
+	document.getElementById('desplieguei').style.display = 'block';
+	document.getElementById('detallei').style.display = 'none';
+	}
+
+function Mostrari() {
+	document.getElementById('ocultoi').style.display = 'block';
+	document.getElementById('desplieguei').style.display = 'none';
+	document.getElementById('detallei').style.display = 'block';
+}
+
+function ocultarw() {
+	document.getElementById('ocultow').style.display = 'none';
+	document.getElementById('desplieguew').style.display = 'block';
+	document.getElementById('detallew').style.display = 'none';
+	}
+
+function Mostrarw() {
+	document.getElementById('ocultow').style.display = 'block';
+	document.getElementById('desplieguew').style.display = 'none';
+	document.getElementById('detallew').style.display = 'block';
+}
+
+function ocultarg() {
+	document.getElementById('ocultog').style.display = 'none';
+	document.getElementById('despliegueg').style.display = 'block';
+	document.getElementById('detalleg').style.display = 'none';
+	}
+
+function Mostrarg() {
+	document.getElementById('ocultog').style.display = 'block';
+	document.getElementById('despliegueg').style.display = 'none';
+	document.getElementById('detalleg').style.display = 'block';
+}
+
+function ocultarm() {
+	document.getElementById('ocultom').style.display = 'none';
+	document.getElementById('desplieguem').style.display = 'block';
+	document.getElementById('detallem').style.display = 'none';
+	}
+
+function Mostrarm() {
+	document.getElementById('ocultom').style.display = 'block';
+	document.getElementById('desplieguem').style.display = 'none';
+	document.getElementById('detallem').style.display = 'block';
+}
+
+function ocultarh() {
+	document.getElementById('ocultoh').style.display = 'none';
+	document.getElementById('despliegueh').style.display = 'block';
+	document.getElementById('detalleh').style.display = 'none';
+	}
+
+function Mostrarh() {
+	document.getElementById('ocultoh').style.display = 'block';
+	document.getElementById('despliegueh').style.display = 'none';
+	document.getElementById('detalleh').style.display = 'block';
+}
+
+function ocultarr() {
+	document.getElementById('ocultor').style.display = 'none';
+	document.getElementById('desplieguer').style.display = 'block';
+	document.getElementById('detaller').style.display = 'none';
+	}
+
+function Mostrarr() {
+	document.getElementById('ocultor').style.display = 'block';
+	document.getElementById('desplieguer').style.display = 'none';
+	document.getElementById('detaller').style.display = 'block';
+}
